d95c9c65718f183fce419f11ee16a7e3ab488537
Grant Hall <grantclone@gmail.com>
2024-10-29 20:33:40 -0700

    update aws helper logic to reflect new aws storage

--- helpers/aws_video_helper.rb
+++ helpers/aws_video_helper.rb
@@ -1,58 +1,55 @@
 require 'aws-sdk-s3'
 require 'aws-sdk-cloudfront'
+require 'csv'
 
 module AwsVideoHelper
   VIDEO_FORMATS = ['.mp4', '.mov', '.avi', '.wmv', '.flv', '.mkv', '.webm', '.m3u8']
 
-  def fetch_aws_videos(folder = nil)
+  def fetch_aws_videos
     s3_client = Aws::S3::Client.new(
       region: ENV['AWS_REGION'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     )
 
+    # Ensure we get the full CloudFront URL
+    cloudfront_url = ENV['CLOUDFRONT_URL'].to_s.strip
+    cloudfront_url = cloudfront_url.split.last if cloudfront_url.include?("\n")
+
     begin
       response = s3_client.list_objects_v2(
-        bucket: ENV['AWS_BUCKET_NAME']
+        bucket: ENV['AWS_BUCKET_NAME'],
+        prefix: 'videos/'
       )
 
-      videos = response.contents.map do |object|
-        # Check if file extension matches any video format
-        next unless VIDEO_FORMATS.any? { |format| object.key.downcase.end_with?(format) }
+      videos = response.contents
+        .select { |obj| obj.key.end_with?('original.mp4') }
+        .map do |video_object|
+          folder_path = File.dirname(video_object.key)
+          folder_name = folder_path.split('/')[1] # Gets "001" from "videos/001"
 
-        video_id = File.basename(object.key, '.*')
-        extension = File.extname(object.key).downcase
+          # Check for thumbnail
+          thumbnail_key = "#{folder_path}/thumbnail.png"
+          thumbnail_exists = response.contents.any? { |obj| obj.key == thumbnail_key }
 
-        {
-          'id' => video_id,
-          'name' => video_id.gsub('_', ' ').capitalize,
-          'description' => '',
-          'url' => "#{ENV['CLOUDFRONT_URL']}/#{object.key}",
-          'size' => format_size(object.size),
-          'type' => extension[1..-1],  # Remove the dot from extension
-          'created_at' => object.last_modified
-        }
-      end.compact
+          # Build URLs
+          video_url = File.join(cloudfront_url, video_object.key)
+          thumbnail_url = thumbnail_exists ? File.join(cloudfront_url, thumbnail_key) : nil
+
+          {
+            'id' => folder_name,
+            'name' => folder_name,
+            'description' => nil,
+            'url' => video_url,
+            'thumbnail_url' => thumbnail_url,
+            'type' => 'mp4'
+          }
+        end
 
       { 'data' => videos }
     rescue => e
       puts "Error: #{e.message}"
       { 'data' => [] }
     end
-  end
-
-  private
-
-  def format_size(size_in_bytes)
-    units = ['B', 'KB', 'MB', 'GB']
-    unit_index = 0
-    size = size_in_bytes.to_f
-
-    while size > 1024 && unit_index < units.length - 1
-      size /= 1024
-      unit_index += 1
-    end
-
-    "#{size.round(1)} #{units[unit_index]}"
   end
 end
